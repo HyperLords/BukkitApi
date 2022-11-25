18291291d51d2ba4f477d6ba04e290793180db18
HyperLords <106620625+HyperLords@users.noreply.github.com>
2022-11-25 22:24:57 +0300

    Delete BukkitApi directory

--- BukkitApi/src/main/java/org/BukkitApi/main/BukkitUtiles/MesageUtils/GetColoredMessage.java
+++
@@ -1,9 +0,0 @@
-package org.BukkitApi.main.BukkitUtiles.MesageUtils;
-
-import net.md_5.bungee.api.ChatColor;
-
-public class GetColoredMessage {
-	public static String getColoredMessage(String message) {
-		return ChatColor.translateAlternateColorCodes('&', message);
-	}
-}
--- BukkitApi/src/main/java/org/BukkitApi/main/BukkitUtiles/MesageUtils/GetUTFMessage.java
+++
@@ -1,9 +0,0 @@
-package org.BukkitApi.main.BukkitUtiles.MesageUtils;
-
-import org.apache.commons.lang.StringEscapeUtils;
-
-public class GetUTFMessage {
-	public static String getColoredMessage(String message) {
-		return message=StringEscapeUtils.escapeJava(message);
-	}
-}
